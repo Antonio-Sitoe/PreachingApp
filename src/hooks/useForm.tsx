ac578d80db9b5a31b5c8ea50c0d65ac284cda992
Antonio Sitoe <antoniositoehl@gmail.com>
2023-08-03 11:43:59 +0200

    eas build configuration

--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -2,10 +2,10 @@ import { ReportType } from '@/@types/enums'
 import { useState } from 'react'
 
 export function useForm(
-  initialValue: number,
+  initialValue: number | string,
   type: ReportType | boolean = false,
 ) {
-  const [value, setValue] = useState(initialValue || 0)
+  const [value, setValue] = useState(initialValue || '')
 
   function onchange(value: number) {
     if (type === ReportType.minutes && value >= 59) return
@@ -13,14 +13,16 @@ export function useForm(
   }
 
   function inCrementValue() {
-    if (type === ReportType.minutes && value >= 59) return
+    if (typeof value === 'number') {
+      if (type === ReportType.minutes && value >= 59) return
 
-    setValue((previewValue) => previewValue + 1)
+      setValue((previewValue) => Number(previewValue) + 1)
+    }
   }
   function decrementValue() {
     setValue((previewValue) => {
-      if (previewValue > 0) return previewValue - 1
-      return 0
+      if (Number(previewValue) > 0) return Number(previewValue) - 1
+      return ''
     })
   }
 
